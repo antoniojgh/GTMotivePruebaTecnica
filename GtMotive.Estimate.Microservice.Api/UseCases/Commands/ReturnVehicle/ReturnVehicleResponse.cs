c1a902c7552b51e191c926679f6b8a5c914faa5e
Antonio Javier Garrido <antoniojaviergr@gmail.com>
2026-03-14 12:26:26 +0100

    Cambios a constructores primarios

--- GtMotive.Estimate.Microservice.Api/UseCases/Commands/ReturnVehicle/ReturnVehicleResponse.cs
+++ GtMotive.Estimate.Microservice.Api/UseCases/Commands/ReturnVehicle/ReturnVehicleResponse.cs
@@ -1,28 +1,19 @@
-using System;
+﻿using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace GtMotive.Estimate.Microservice.Api.UseCases.Commands.ReturnVehicle
 {
     /// <summary>Return vehicle response DTO.</summary>
-    public sealed class ReturnVehicleResponse
+    /// <param name="vehicleId">The vehicle identifier.</param>
+    /// <param name="returnDate">The date the vehicle was returned.</param>
+    public sealed class ReturnVehicleResponse(Guid vehicleId, DateTime returnDate)
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ReturnVehicleResponse"/> class.
-        /// </summary>
-        /// <param name="vehicleId">The vehicle identifier.</param>
-        /// <param name="returnDate">The date the vehicle was returned.</param>
-        public ReturnVehicleResponse(Guid vehicleId, DateTime returnDate)
-        {
-            VehicleId = vehicleId;
-            ReturnDate = returnDate;
-        }
-
         /// <summary>Gets the vehicle identifier.</summary>
         [Required]
-        public Guid VehicleId { get; }
+        public Guid VehicleId { get; } = vehicleId;
 
         /// <summary>Gets the date the vehicle was returned.</summary>
         [Required]
-        public DateTime ReturnDate { get; }
+        public DateTime ReturnDate { get; } = returnDate;
     }
 }
